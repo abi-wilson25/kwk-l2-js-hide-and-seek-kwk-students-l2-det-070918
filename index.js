96a42bac44a99c55460ac1105e13649c769e708f
abi-wilson25 <agibailw@gmail.com>
2018-07-12 17:58:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function nestedTarget(){
 }
 
 function increaseRankBy(n){
-  var newNumber = 
-  return document.querySelector(parseInt('ul.ranked-list') + n)
+  var newNumber =document.querySelector(parseInt('ul.ranked-list') + n) 
+  return 
 }
