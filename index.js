3bc678db5b722f47e6193f874cd8b42545d13323
abi-wilson25 <agibailw@gmail.com>
2018-07-12 18:28:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function increaseRankBy(n){
  for (let i = 0, l = rankedLists.length; i < l; i++){
    let children = rankedLists[i].children
    
-   for (let j = 0, k = childre.length; j < k; j++){
+   for (let j = 0, k = children.length; j < k; j++){
      children[j].innerHTML = parseInt(children[j].innerHTML) + n
    }
  }
